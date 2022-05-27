2db5dfa5d96662dca3225bf6193caf6b60a1417f
dangilcal <danielgilcalahorrano@gmail.com>
2022-05-27 15:03:01 +0200

    Funcionalidad en nav-bar y diseño-beta logo

--- WEB/src/app/components/nav-bar/nav-bar.component.ts
+++ WEB/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tokenHelp } from 'src/app/helper/tokenHelp';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  constructor() {}
+  constructor(private _token: tokenHelp) {
+    this.existeToken = null;
+  }
+  existeToken: boolean | null;
+  ngOnInit(): void {
+    if (this._token.getCookie() == "") {
+      this.existeToken = false;
+    } else {
+      this.existeToken = true;
+    }
+  }
 
-  ngOnInit(): void {}
+  cerrarSesion() {
+    this._token.closeToken();
+    window.location.reload();
+  }
 }
